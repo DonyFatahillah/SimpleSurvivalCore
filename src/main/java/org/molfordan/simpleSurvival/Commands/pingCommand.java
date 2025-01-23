7bd747f0eebcf7292d0129d5100e5fafc56a2a33
DonyFatahillah <dony.asyabani09@gmail.com>
2025-01-23 13:10:29 +0700

    some updates

--- src/main/java/org/molfordan/simpleSurvival/Commands/pingCommand.java
+++ src/main/java/org/molfordan/simpleSurvival/Commands/pingCommand.java
@@ -1,5 +1,6 @@
 package org.molfordan.simpleSurvival.Commands;
 
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -18,10 +19,44 @@ public class pingCommand implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String s, String[] args) {
 
-        Player player = (Player) sender;
-        int ping  = player.getPing();
-        player.sendMessage(color.GREEN + "Your ping is " + ping + "ms");
+        if(!(sender instanceof Player)){
+            sender.sendMessage("you are not allowed to execute this command!");
+            return true;
+        }
 
+        if (args.length == 0) {
+
+            Player player = (Player) sender;
+            int ping = player.getPing();
+            player.sendMessage(color.GREEN + "Your ping is " + ping + "ms");
+            return true;
+        }
+
+
+
+        if (args.length == 1){
+
+            Player target = Bukkit.getPlayer(args[0]);
+            if(args[0].equals(target.getName())) {
+
+                if (target != null) {
+                    int ping = target.getPing();
+                    sender.sendMessage(color.GREEN + target.getName() + "'s ping is " + ping + "ms");
+                    return true;
+                } else {
+                    sender.sendMessage(color.RED + "that player is not online!");
+
+                }
+
+                return true;
+            } else {
+                sender.sendMessage(color.GREEN+"Usage: /ping <playerName>");
+
+            }
+
+            return true;
+
+        }
 
 
         return true;
